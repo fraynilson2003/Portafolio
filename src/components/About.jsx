5332b1578734f3c406ae8356a2d12c2bafc432c1
fraynilson2003 <fraynilson2003@gmail.com>
2023-03-16 20:05:41 -0500

    2/3

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,70 @@
 import React from 'react';
+import CountUp from "react-countup"
+import { useInView } from "react-intersection-observer"
+//motion
+import { motion } from "framer-motion"
+import { fadeIn } from '../variants';
+//
+
 
 const About = () => {
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  })
+
+
   return (
-    <div id="about" className="section">
-      About
+    <div id="about" className="section " ref={ref}>
+      <div className="container mx-auto">
+
+        <div className="flex flex-col gap-y-10 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen">
+
+          {/*image */}
+          <motion.div
+            variants={fadeIn("right", 0.25 )} 
+            initial="hidden" 
+            whileInView={"show"} 
+            viewport={{once: false, amount:0.3}}  
+            className="flex-1 bg-about bg-contain bg-no-repeat max-h-[280px] h-[200px] mix-blend-lighten bg-top lg:h-[500px] lg:max-h-[500px]">
+          </motion.div>
+          
+          {/*text */}
+          <motion.div
+            variants={fadeIn("left", 0.3 )} 
+            initial="hidden" 
+            whileInView={"show"} 
+            viewport={{once: false, amount:0.3}}  
+            className="flex-1"
+          >
+            <h2 className="h2 text-accent">Mas sobre.</h2>
+            <h3 className="h3 mb-4">Un desarrollador Back-end, Front-end y .Net</h3>
+            <p className="mb-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Alias tenetur architecto atque neque distinctio quam eius officiis. Odio laudantium dolorem provident ab, iste culpa quaerat tempora quibusdam architecto perspiciatis aut.</p>
+
+            {/**Stats */}
+            <div className="flex gap-x-6 lg:gap-x-10 mb-12 mt-4">
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2 ">
+                  {inView?
+                    <CountUp start={0} end={8} duration={2} delay={0.2}/> : null}
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Proyectos completados
+                </div>
+
+              </div>
+            </div>
+
+            <div className="flex gap-x-8 justify-center lg:justify-start">
+              <button className="btn btn-lg  ">Contactame</button>
+              <a href="#" className="flex text-gradient btn-link items-center">
+                Mi portafolio
+              </a>
+              </div>        
+
+          </motion.div>
+        </div>
+      </div>
+
     </div>
 )}
 
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,11 +1,89 @@
-import React from 'react';
+import React from "react";
+//icon
+import { BsArrowUpRight } from "react-icons/bs";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
+
+//services
+const services = [
+  {
+    name: "Back-end Developer",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil quisquam cumque, illo eos pariatur necessitatibus sequi inventore est, vitae reprehenderit, adipisci quia nostrum modi minus quibusdam asperiores voluptatibus iste expedita.",
+    link: "ver mas"
+  },
+  {
+    name: "Front-end Developer",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil quisquam cumque, illo eos pariatur necessitatibus sequi inventore est, vitae reprehenderit, adipisci quia nostrum modi minus quibusdam asperiores voluptatibus iste expedita.",
+    link: "ver mas"
+  },
+  {
+    name: ".NET",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil quisquam cumque, illo eos pariatur necessitatibus sequi inventore est, vitae reprehenderit, adipisci quia nostrum modi minus quibusdam asperiores voluptatibus iste expedita.",
+    link: "ver mas"
+  },
+]
 
 const Services = () => {
+
+
+
   return (
     <div id="services" className="section">
-      Services
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row">
+
+          {/**texto */}
+          <motion.div 
+            variants={fadeIn("right", 0.25 )} 
+            initial="hidden" 
+            whileInView={"show"} 
+            viewport={{once: false, amount:0.3}}            
+            className="flex-1 lg:bg-services lg:bg-botttom bg-no-repeat bg mix-blend-lighten bg-center mb-10 lg:mb-0"
+          >
+            <h2 className="h2 to-accent mb-6 ">¿Que necesitas?</h2>
+            <h3 className="h3 max-w-[400px] mb-13">Soy un programador que le gusta los desafios. <br /> Tambien trabajar en equipo</h3>
+
+            <button className="btn btn-sm">Mira mi trabajo</button>
+          </motion.div>
+
+          {/**services */}
+          <motion.div
+            variants={fadeIn("left", 0.3 )} 
+            initial="hidden" 
+            whileInView={"show"} 
+            viewport={{once: false, amount:0.3}}
+          >
+            {/**list services */}
+            <div>
+
+              {services?.length && services.map((se, ind)=>
+                <div className="border-b border-white/20 h-[146px] h-auto mb-[34px] flex" key={ind}>
+
+                  <div className="max-w-[455px]">
+
+                    <div className="flex flex-row justify-between">
+                      <h4 className="text-[20px] tracking-wider font-primary font-semibold mb-5">{se.name}</h4>
+
+                      <a href="#" className="btn w-8 h-8 mb-[5px] flex items-center justify-center">
+                        <BsArrowUpRight/>
+                      </a>
+
+                    </div>
+
+                    <p className="font-secondary leading-tight text-justify">{se.description}</p>
+                  </div>
+
+                </div>
+              )}
+
+            </div>
+          </motion.div>
+
+        </div>
+
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Services;
